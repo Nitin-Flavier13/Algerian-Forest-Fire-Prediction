a5b79153e7e522dacf81ef5bc331f9f8905abb9e
Nitin Flavier <leviackerman0013@gmail.com>
2024-11-29 14:16:48 +0530

    Initial commit

---
+++ application.py
@@ -0,0 +1,40 @@
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from flask import Flask, jsonify, render_template, request
+
+application = Flask(__name__)
+app = application
+
+# import our model and scaler
+elasticNetCV = pickle.load(open('models/elasticNetCV.pkl','rb'))
+standardScaler = pickle.load(open('models/scaler.pkl','rb'))
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/predictdata",methods=['GET','POST'])
+def predict():
+    if request.method == "POST":
+        Temperature = float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
+
+        input_data = standardScaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+
+        pred_fwi = elasticNetCV.predict(input_data)
+
+        return render_template('home.html',results=pred_fwi)
+    else:
+        return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True)
